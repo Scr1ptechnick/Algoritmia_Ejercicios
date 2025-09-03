8b1ba1174de120d652ba62281d09285d10c6427b
Balthazar <Gohantk@gmail.com>
2025-09-03 17:51:10 -0500

    Mi primer commit de proyecto

---
+++ Actividad1 - copia (3).py
@@ -0,0 +1,10 @@
+numero1 = int(input("ingresa un numero......:"))
+#ahora vamos ha hacer la funcion SI para comparar
+if numero1 % 2 == 0:
+    print("El numero es par")
+    print(f"El numero {numero1} es par")
+    print("El numero ", numero1," es par")
+else:
+    print("El numero es impar")
+    print(f"El numero {numero1} es impar")
+    print("El numero " ,numero1,"  es impar")
